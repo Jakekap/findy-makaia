6dc6c6deb426f136e1df1d7e993da60a299fa783
Josman Santiago Muñoz Mera <josmansant@unicauca.edu.co>
2023-11-04 10:22:43 -0500

    Se agregan complemento carrousel  y pages post

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import HomeFeed from '../../components/homeFeed/HomeFeed'
-import './feed.css'
+import './feed.scss'
+
 const Feed = () => {
   return (
     <div>
